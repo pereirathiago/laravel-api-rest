c11948faa472dccf7ebd2344dab1688f5481d294
Thiago Pereira <pereira.tp.thiago@gmail.com>
2023-08-03 22:34:57 -0300

    feat: aula 11 - configurando base para API

--- primeira_api/routes/api.php
+++ primeira_api/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +18,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('test', function(Request $request) {
+
+    // dd($request->headers->get('Authorization'));
+    // dd($request->headers->all());
+
+    $response = new Response(['msg' => 'Minha primeira resposta de APi']);
+    $response->header('Content-Type', 'application/json');
+    return $response;
+});
+
+// Products route
+Route::get('/products', function() {
+    return Product::all();
 });
